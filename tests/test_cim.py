32693de39b3a370cd431b96817f11b2db0914e97
Erik Schamper <1254028+Schamper@users.noreply.github.com>
2025-04-03 11:00:16 +0200

    Fix regression for reading unknown data types (#32)

--- tests/test_cim.py
+++ tests/test_cim.py
@@ -8,3 +8,8 @@ from dissect.cim.cim import CIM
 def test_cim(index_bin: BinaryIO, objects_bin: BinaryIO, mappings_bin: list[BinaryIO]) -> None:
     repo = CIM(index_bin, objects_bin, mappings_bin)
     assert repo
+
+    subscription_ns = repo.root.namespace("subscription")
+    binding = next(subscription_ns.class_("__filtertoconsumerbinding").instances)
+    consumer = subscription_ns.query(binding.properties["Consumer"].value)
+    assert consumer.properties["SourceName"].value == "Service Control Manager"
